161358eba214992b94c8cc3044c748179de150a7
Merker <3deamerker@gmail.com>
2018-12-30 12:00:24 -0500

    Initial Upload

---
+++ A-4E-C/Options/optionsData.lua
@@ -0,0 +1,16 @@
+
+-- default values
+-- defaults =
+-- {
+	-- altTrimmingMethod = {
+        -- __value__ = false,
+        -- control = 'check',
+        hidden = true,
+        -- },
+-- }
+
+-- function init(a_lang, a_env)
+	-- if (a_lang == 'ru') then
+		
+	-- end
+-- end
