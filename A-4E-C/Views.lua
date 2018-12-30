d96d4fb401a48eb942c59a1065b12b6246faa583
Merker <3deamerker@gmail.com>
2018-12-30 12:05:36 -0500

    Initial Upload

---
+++ A-4E-C/Views.lua
@@ -0,0 +1,170 @@
+ViewSettings = {
+
+	Cockpit = {
+
+	[1] = {-- player slot 1
+		--CockpitLocalPoint      = {-1.6,1.1,0.000000},
+		CameraViewAngleLimits  = {30,120}, -- FOV Mini Maxi
+		CameraAngleRestriction = {false,90,0.4},
+		CameraAngleLimits      = {175,-80,110}, -- View right/left, View down/up (Vue Droite/gauche Vue, Bas, Haut)
+		limits_6DOF            = {x = {-0.05,0.45},y = {-0.3,0.1},z = {-0.17,0.17},roll = 70.000000}, -- movement back,front bottom,top right,left
+		--EyePoint               = {0.1000000,0.000000,0.000000},
+		ShoulderSize 		   = 0.25,
+																						
+	},	
+
+	}, -- Cockpit 
+
+	Chase = {
+		LocalPoint      = {-10.000000,1.100000,-0.300000}, -- Vue F4
+		AnglesDefault   = {180.000000,-8.000000},
+
+	}, -- Chase 
+
+	Arcade = {
+		LocalPoint      = {-20.000000,5.000000,0.000000},
+		AnglesDefault   = {0.000000,-8.000000},
+	}, -- Arcade 
+
+}
+
+
+
+SnapViews = {
+[1] = {-- player slot 1
+	[1] = {
+		viewAngle = 70.611748,--FOV
+		hAngle	 = -1.240272,
+		vAngle	 = 0.850250,
+		x_trans	 = 0.164295,
+		y_trans	 = -0.074373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[2] = {
+		viewAngle = 32.704346,--FOV
+		hAngle	 = 25.696522,
+		vAngle	 = -34.778103,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+
+	},
+
+	[3] = {
+		viewAngle = 32.704346,--FOV
+		hAngle	 = 0.000000,
+		vAngle	 = -47.845268,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[4] = {
+		viewAngle = 36.106045,--FOV
+		hAngle	 = -28.878576,
+		vAngle	 = -36.780628,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[5] = {
+		viewAngle = 88.727844,--FOV
+		hAngle	 = 128.508865,
+		vAngle	 = 13.131046,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[6] = {
+		viewAngle = 41.928593,--FOV
+		hAngle	 = 0.000000,
+		vAngle	 = -4.630446,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[7] = {
+		viewAngle = 88.727844,--FOV
+		hAngle	 = -128.508865,
+		vAngle	 = 13.131046,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[8] = {
+		viewAngle = 88.727844,--FOV
+		hAngle	 = 81.648369,
+		vAngle	 = -9.500000,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[9] = {
+		viewAngle = 88.727844,--FOV
+		hAngle	 = 0.000000,
+		vAngle	 = 34.180634,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[10] = {
+		viewAngle = 88.727844,--FOV
+		hAngle	 = -80.997551,
+		vAngle	 = -9.500000,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+	[11] = {--look at left  mirror
+		viewAngle = 56.032040,--FOV
+		hAngle	 = 14.803060,
+		vAngle	 = 3.332499,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+
+	},
+
+	[12] = {--look at right mirror
+		viewAngle = 56.032040,--FOV
+		hAngle	 = -14.414484,
+		vAngle	 = 3.332499,
+		x_trans	 = 0.264295,
+		y_trans	 = -0.064373,
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+
+	},
+
+	[13] = {--default view
+		viewAngle = 75.000000,-- FOV when spawning in pit (auto dezoom) (FOV arrivée Cockpit (une fois dézoom fait par DCS) ) 95
+		hAngle	 = 0.000000,
+		vAngle	 = -13.000000, -- horizontal cockpit view (Vue horizontale cockpit) -10
+		x_trans	 = 0,
+		y_trans	 = 0,  -- base height of the cockpit view (Hauteur de base de la vue cockpit)
+		z_trans	 = 0.000000,
+		rollAngle = 0.000000,
+	},
+
+},
+
+}
