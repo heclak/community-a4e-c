a25ed7567425f8433d8d0059d61042a3e69c48df
Merker <3deamerker@gmail.com>
2018-12-30 12:00:01 -0500

    Initial Upload

---
+++ A-4E-C/Missions/EN/QuickStart/quickstart.lua
@@ -0,0 +1,28 @@
+planes = {
+    { 
+    name = _('Cold Start'),
+    file = 'a4e_cold_start.miz',
+	},
+	{ 
+    name = _('Free Flight'),
+    file = 'a4e_free_flight.miz',
+    },
+    { 
+    name = _('Ground Attack'),
+    file = 'a4e_ground_attack.miz',
+    },
+    { 
+    name = _('Dogfight'),
+    file = 'a4e_dogfight.miz',
+    },
+	{ 
+    name = _('Takeoff'),
+    file = 'a4e_takeoff.miz',
+    },
+	{ 
+    name = _('Landing'),
+    file = 'a4e_landing.miz',
+    },
+}
+
+--Zilch, please add mission examples here.
---
+++ A-4E-C/Missions/EN/Training/lessons.lua
@@ -0,0 +1,78 @@
+lessons = 
+{
+	{
+        ["file"] = "01- Su-25T Start Up.miz",
+        ["name"] = "Aircraft Start-Up, Taxi, and Takeoff",
+        ["description"] = "In this lesson, we will practice starting the Su-25T from the ramp, taxi to the runway, and takeoff.",
+    },
+	{
+        ["file"] = "02- Su-25T Flight and Navigation.miz",
+        ["name"] = "Basic Flight Controls and Navigation",
+        ["description"] = "In this lesson, we will learn about the basic flight controls of the Su-25T, how to navigate between waypoints, and how to select autopilot modes.",
+    },
+	{
+        ["file"] = "03- Su-25T Landing - Easy.miz",
+        ["name"] = "Landing - Easy",
+        ["description"] = "In this lesson, we will learn how to navigate to an airfield and land at it under good conditions.",
+    },
+	{
+        ["file"] = "04- Su-25T Landing - Difficult.miz",
+        ["name"] = "Landing - Difficult",
+        ["description"] = "In this lesson, we will learn how to navigate to an airfield and land at it under poor conditions.",
+    },
+	{
+        ["file"] = "05- Su-25T CCIP Mode Unguided Bombs.miz",
+        ["name"] = "CCIP Mode, Unguided Bombing",
+        ["description"] = "In this lesson, we are going to learn how to deliver unguided bombs using Continuously Computed Impact Point (CCIP) mode.",
+    },
+	{
+        ["file"] = "06- Su-25T CCRP Mode Unguided Bombs.miz",
+        ["name"] = "CCRP Mode, Unguided Bombing",
+        ["description"] = "In this lesson, we are going to learn how to deliver unguided bombs using Continuously Computed Release Point (CCRP) mode.",
+    },
+	{
+        ["file"] = "07- Su-25T KMGU Submunitions Dispenser.miz",
+        ["name"] = "KMGU Submunitions Dispenser",
+        ["description"] = "In this lesson, we will learn to employ the KMGU submunitions dispenser.",
+    },
+    {
+        ["file"] = "08- Su-25T Unguided Rockets.miz",
+        ["name"] = "Unguided Rockets",
+        ["description"] = "In this lesson, we are going to learn how to employ unguided rockets.",
+    },
+	{
+        ["file"] = "09- Su-25T Internal 30 mm Cannon.miz",
+        ["name"] = "Internal Cannon",
+        ["description"] = "In this lesson, we will learn to use the internal, twin barrel GSh-20 30mm cannon.",
+    },
+	{
+        ["file"] = "10- Su-25T Gun Pods.miz",
+        ["name"] = "Gun Pods",
+        ["description"] = "In this lesson, we will learn to use the SPPU-22-1 gun pods.",
+    },
+	{
+        ["file"] = "11- Su-25T Shkval Assisting Guided Bombs and Missiles.miz",
+        ["name"] = "Shkval Sensor with Kh-29T and KAB-500Kr",
+        ["description"] = "In this lesson, you will learn how to use the Shkval sensor, the Kh-29T guided-missile and the KAB-500Kr guided-bomb.",
+    },
+	{
+        ["file"] = "12- Su-25T Kh-29L, Kh-25ML and S-25L Laser Guided Missiles.miz",
+        ["name"] = "Kh-29L, Kh-25ML, and S-25L Laser-Guided Missiles",
+        ["description"] = "In this lesson, you will learn how to employ the Kh-29L, Kh-25ML, and S-25L laser-guided missiles.",
+    },
+	{
+        ["file"] = "13- Su-25T Vikhr Laser Guided Missile and Mercury Pod.miz",
+        ["name"] = "Vikhr Laser Guided Missile and Mercury Pod",
+        ["description"] = "In this lesson, you will learn how to use the Vikhr laser-guided anti-tank missile and the Mercury night vision pod.",
+    },
+	{
+        ["file"] = "14- Su-25T Anti-Radiation Missiles, RWR and Countermeasures.miz",
+        ["name"] = "Anti-Radar Missiles, RWR and Countermeasures",
+        ["description"] = "In this lesson, you will learn how to interpret the radar warning receiver, how to use the countermeasure systems of the Su-25T, and how to employ the Kh-58U and Kh-25MPU anti-radar missiles.",
+    },
+    {
+        ["file"] = "15- Su-25T Air-to-Air.miz",
+        ["name"] = "Air-to-Air Missiles",
+        ["description"] = "In this lesson, you will learn to use the R-60M and R-73 infrared-guided, short-range air-to-air missiles.",
+    },
+} -- end of lessons
