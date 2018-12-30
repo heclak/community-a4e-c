f911aa8b2edac10af2cc565dc1ec07efc1cea8cb
Merker <3deamerker@gmail.com>
2018-12-30 11:49:53 -0500

    Initial Upload

---
+++ A-4E-C/cockpit/Scripts/RADAR/Indicator/init.lua
@@ -0,0 +1,49 @@
+dofile(LockOn_Options.common_script_path.."devices_defs.lua")
+dofile(LockOn_Options.common_script_path.."ViewportHandling.lua")
+dofile(LockOn_Options.script_path.."utils.lua")
+
+startup_print("radar HUD: load")
+
+indicator_type       = indicator_types.COMMON----------------------
+init_pageID     = 1
+purposes 	   = {render_purpose.GENERAL,render_purpose.HUD_ONLY_VIEW}
+--subset ids
+BASE    = 1
+INDICATION = 2
+
+page_subsets  = {
+[BASE]    		= LockOn_Options.script_path.."RADAR/Indicator/base_page.lua",
+[INDICATION]    = LockOn_Options.script_path.."RADAR/Indicator/indication_page.lua",
+}
+pages = 
+{
+	{
+	 BASE,
+	 INDICATION
+	 },
+}
+
+--[[
+opacity_sensitive_materials =
+{
+"BLOB_TEXTURE"
+}
+
+color_sensitive_materials =
+{
+"BLOB_TEXTURE"
+}
+
+brightness_sensitive_materials =
+{
+"BLOB_TEXTURE"
+}
+
+day_color    = {0, 0, 255/255, 255/255}
+night_color  = {243/255, 116/255, 13/255, 255/255}
+--]]
+
+update_screenspace_diplacement(SelfWidth/SelfHeight,false)
+dedicated_viewport_arcade = dedicated_viewport
+
+startup_print("radar HUD: load end")
