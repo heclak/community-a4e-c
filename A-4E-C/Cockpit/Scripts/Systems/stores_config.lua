adb631206b304facaabd7509201d5c4d8176f05d
Hector Lee <heclak+github@gmail.com>
2019-01-06 19:02:58 +0800

    Reworked AWRS ripple modes for bombs and rockets to match real world aircraft. In ripple mode, quantity select will determine number of ripples released for ripple single and ripple pairs. Weapon release will stop after last ripple even if pickle or rocket trigger is held. LABS annunciator will illuminate and tone will play when pickle or rocket trigger is held down. Tone and light will stop in ripple step and ripple pairs mode at end of ripple sequence or weapon stations is empty. CBU weapons (SUU-7) will now release quantity of weapons by tubeloads as per real world. Number of tubeloads released is based on specific CBU loaded.

--- A-4E-C/Cockpit/Scripts/Systems/stores_config.lua
+++ A-4E-C/Cockpit/Scripts/Systems/stores_config.lua
@@ -3,7 +3,14 @@ dofile(LockOn_Options.common_script_path.."../../../Database/wsTypes.lua")
 ------------------------------------------------
 ----------------  CONSTANTS  -------------------
 ------------------------------------------------
-
+cbu_mult = 5
+dispenser_data =
+{
+    --use shapename,         bomblet,          bomblet_count
+    ["{CBU-1/A}"]          = { bomblet = BLU_4B, bomblet_count = math.floor(509/cbu_mult), number_of_tubes = 19, tubes_per_pulse = 2 },
+    ["{CBU-2/A}"]          = { bomblet = BLU_3B, bomblet_count = math.floor(360/cbu_mult), number_of_tubes = 17, tubes_per_pulse = 1 },
+    ["{CBU-2B/A}"]         = { bomblet = BLU_3B, bomblet_count = math.floor(409/cbu_mult), number_of_tubes = 19, tubes_per_pulse = 2 },
+}
 
 ------------------------------------------------
 --------------  END CONSTANTS  -----------------
