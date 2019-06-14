52f7896b0959e03068eaf396f45dbbcccae35b12
Hector Lee <heclak+github@gmail.com>
2019-06-15 01:22:07 +0800

    Added new quick start missions
    - New carrier launch mission
    - Checked and reworked Caucasus missions
    - Added new set of missions for Persian Gulf

--- A-4E-C/Missions/EN/QuickStart/quickstart.lua
+++ A-4E-C/Missions/EN/QuickStart/quickstart.lua
@@ -1,28 +1,54 @@
 planes = {
-    { 
-    name = _('Cold Start'),
-    file = 'a4e_cold_start.miz',
-	},
-	{ 
-    name = _('Free Flight'),
-    file = 'a4e_free_flight.miz',
-    },
-    { 
-    name = _('Ground Attack'),
-    file = 'a4e_ground_attack.miz',
-    },
-    { 
-    name = _('Dogfight'),
-    file = 'a4e_dogfight.miz',
-    },
-	{ 
-    name = _('Takeoff'),
-    file = 'a4e_takeoff.miz',
-    },
-	{ 
-    name = _('Landing'),
-    file = 'a4e_landing.miz',
-    },
-}
 
---Zilch, please add mission examples here.
+    -- Caucasus
+    {
+        name = _("Cold Start"),
+        file = "Caucasus A-4E-C Cold Start.miz"
+    },
+    {
+        name = _("Takeoff"),
+        file = "Caucasus A-4E-C Takeoff.miz"
+    },
+    {
+        name = _("Carrier Launch"),
+        file = "Caucasus A-4E-C Carrier Start.miz"
+    },
+    {
+        name = _("Free Flight"),
+        file = "Caucasus A-4E-C Free Flight.miz"
+    },
+
+    -- Persian Gulf
+    {
+        name = _("Cold Start"),
+        file = "Persian Gulf A-4E-C Cold Start.miz"
+    },
+    {
+        name = _("Takeoff"),
+        file = "Persian Gulf A-4E-C Takeoff.miz"
+    },
+    {
+        name = _("Carrier Launch"),
+        file = "Persian Gulf A-4E-C Carrier Start.miz"
+    },
+    {
+        name = _("Free Flight"),
+        file = "Persian Gulf A-4E-C Free Flight.miz"
+    },
+
+
+
+    -- Possible future missions
+    -- {
+    --     name = _("Ground Attack"),
+    --     file = "a4e_ground_attack.miz"
+    -- },
+    -- {
+    --     name = _("Dogfight"),
+    --     file = "a4e_dogfight.miz"
+    -- },
+    -- {
+    --     name = _("Landing"),
+    --     file = "a4e_landing.miz"
+    -- }
+}
