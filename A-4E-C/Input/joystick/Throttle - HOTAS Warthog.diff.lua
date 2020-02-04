60f5fc8ab2efc9df20c3070775490154eff560e3
Hector Lee <heclak+github@gmail.com>
2020-02-04 21:10:06 +0800

    refactor(input): Changed Warthog Throttle to use diff file
    Changing to diff.lua files will allow for better management of keybind files. Only the main joystick and keyboard lua files will need to be updated in the future.

---
+++ A-4E-C/Input/joystick/Throttle - HOTAS Warthog.diff.lua
@@ -0,0 +1,95 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["name"] = "Pitch",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Y",
+				},
+			},
+		},
+		["a2002cdnil"] = {
+			["name"] = "Roll",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_X",
+				},
+			},
+		},
+		["a3123cd35"] = {
+			["name"] = "Rudder Axis",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d10001pnilu10000cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN22",
+				},
+			},
+			["name"] = "Flaps Up else Stop (HOTAS)",
+		},
+		["d10002pnilu10000cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Flaps Down else Stop (HOTAS)",
+		},
+		["d10006pnilu10007cdnilvdnilvpnilvunil"] = {
+			["name"] = "Weapon Fire",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+		["d10008pnilu10009cdnilvdnilvpnilvunil"] = {
+			["name"] = "Weapon Release",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+		},
+		["d10093pnilu10093cdnilvd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Master Exterior Lights - MOMENTARY ON else OFF",
+		},
+		["d10093pnilu10093cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Master Exterior Lights - ON else OFF",
+		},
+		["d147pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Speedbrake OPEN",
+		},
+		["d148pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Speedbrake CLOSE",
+		},
+	},
+}
+return diff
