a6d81da21e293e496dd41dc024fd52a993729157
Joshua Nelson <joshnel123@gmail.com>
2020-09-17 23:32:27 +0100

    Test commit

--- A-4E-C/ExternalFM/FM/Table.h
+++ A-4E-C/ExternalFM/FM/Table.h
@@ -6,7 +6,7 @@
 class Table
 {
 public:
-	Table(const std::vector<double> table, double min, double max) :
+	Table(const std::vector<double>& table, double min, double max) :
 		m_table(table),
 		m_min(min),
 		m_max(max)
@@ -33,34 +33,6 @@ public:
 			return m_table.front();
 		if (upper >= m_table.size())
 			return m_table.back();
-
-		double lowerX = (double)lower * m_dx + m_min;
-		double upperX = (double)upper * m_dx + m_min;
-
-		return (value - lowerX) * ((m_table[upper] - m_table[lower]) / (upperX - lowerX)) + m_table[lower];
-	}
-	double operator()(double value, bool print)
-	{
-
-		if (m_table.size() == 1)
-			return m_table[0];
-
-		double index = (value - m_min) / m_dx;
-
-		int lower = floor(index);
-		int upper = ceil(index);
-
-		if (lower == upper)
-			upper++;
-
-		//printf("lower %d, upper %d\n", lower, upper);
-
-		if (lower < 0)
-			return m_table.front();
-
-		if (upper >= m_table.size())
-			return m_table.back();
-		
 
 		double lowerX = (double)lower * m_dx + m_min;
 		double upperX = (double)upper * m_dx + m_min;
