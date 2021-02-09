843590fdc5392ba7a43b577f0b245b51af692d68
Partario Flynn <partario.flynn@gmail.com>
2021-02-08 21:59:32 -0500

    added quickstart dogfighting missions for Caucasus and Persian Gulf (thanks Cubeboy)

--- A-4E-C/Missions/EN/QuickStart/quickstart.lua
+++ A-4E-C/Missions/EN/QuickStart/quickstart.lua
@@ -18,12 +18,36 @@ planes = {
         file = "Caucasus A-4E-C Free Flight.miz"
     },
     {
-        name = _("Harbour Bombing (by sidekick65)"),
+        name = _("Ground Attack: Harbour"),
         file = "Caucasus A-4E-C Harbour Bombing.miz"
     },
     {
-        name = _("Slalom Course (by sidekick65)"),
+        name = _("Ground Attack: Slalom Course"),
         file = "Caucasus A-4E-C Slalom.miz"
+    },
+    {
+        name = _("Dogfight: A-4E-C (Trained)"),
+        file = "Caucasus A-4E-C Dogfight vs A-4E-C Trained.miz"
+    },
+    {
+        name = _("Dogfight: A-4E-C (Veteran)"),
+        file = "Caucasus A-4E-C Dogfight vs A-4E-C Veteran.miz"
+    },
+    {
+        name = _("Dogfight: F-14A (Trained)"),
+        file = "Caucasus A-4E-C Dogfight vs F-14A Trained.miz"
+    },
+    {
+        name = _("Dogfight: F-14A (Veteran)"),
+        file = "Caucasus A-4E-C Dogfight vs F-14A Veteran.miz"
+    },
+    {
+        name = _("Dogfight: MiG-15 (Trained)"),
+        file = "Caucasus A-4E-C Dogfight vs MiG-15 Trained.miz"
+    },
+    {
+        name = _("Dogfight: MiG-15 (Veteran)"),
+        file = "Caucasus A-4E-C Dogfight vs MiG-15 Veteran.miz"
     },
 
     -- Persian Gulf
@@ -43,6 +67,30 @@ planes = {
         name = _("Free Flight"),
         file = "Persian Gulf A-4E-C Free Flight.miz"
     },
+    {
+        name = _("Dogfight: A-4E-C (Trained)"),
+        file = "Persian Gulf A-4E-C Dogfight vs A-4E-C Trained.miz"
+    },
+    {
+        name = _("Dogfight: A-4E-C (Veteran)"),
+        file = "Persian Gulf A-4E-C Dogfight vs A-4E-C Veteran.miz"
+    },
+    {
+        name = _("Dogfight: F-4E (Trained)"),
+        file = "Persian Gulf A-4E-C Dogfight vs F-4E Trained.miz"
+    },
+    {
+        name = _("Dogfight: F-4E (Veteran)"),
+        file = "Persian Gulf A-4E-C Dogfight vs F-4E Veteran.miz"
+    },
+    {
+        name = _("Dogfight: MiG-19 (Trained)"),
+        file = "Persian Gulf A-4E-C Dogfight vs MiG-19 Trained.miz"
+    },
+    {
+        name = _("Dogfight: MiG-19 (Veteran)"),
+        file = "Persian Gulf A-4E-C Dogfight vs MiG-19 Veteran.miz"
+    },
 
     -- Nevada
     {
@@ -58,17 +106,4 @@ planes = {
         file = "Nevada A-4E-C Free Flight.miz"
     },
 
-    -- Possible future missions
-    -- {
-    --     name = _("Ground Attack"),
-    --     file = "a4e_ground_attack.miz"
-    -- },
-    -- {
-    --     name = _("Dogfight"),
-    --     file = "a4e_dogfight.miz"
-    -- },
-    -- {
-    --     name = _("Landing"),
-    --     file = "a4e_landing.miz"
-    -- }
 }
