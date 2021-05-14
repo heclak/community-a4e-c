9c6f9839e6396d77429fd28315f251ae6e90eee0
Partario Flynn <partario.flynn@gmail.com>
2021-05-14 11:16:38 -0400

    updated external texture material names, livery updates to match
    thanks heclak - the external model has been placed on the google drive binary storage.

--- A-4E-C/Liveries/A-4E-C/Blue Angels no 1/description.lua
+++ A-4E-C/Liveries/A-4E-C/Blue Angels no 1/description.lua
@@ -2,55 +2,55 @@ name = "Blue Angels no. 1"
 countries = {"USA"}
 
 livery = {
-  {"Top", 0, "a4e_blueangels-top-1", true};
+  {"Top", DIFFUSE, "a4e_blueangels-top-1", true};
   {"Top", ROUGHNESS_METALLIC, "a4e_blueangels-top_Roughmet", true};
-  {"Bottom", 0, "a4e_blueangels-bot", true};
+  {"Bottom", DIFFUSE, "a4e_blueangels-bot", true};
   {"Bottom", ROUGHNESS_METALLIC, "a4e_blueangels-bot_Roughmet", true};
-  {"Gear and metal", 0, "a4e_blueangels-bot", true};
-  {"Cockpit", 0, "a4e_blueangels-bot", true};
-  --{"Front",	0, "fus_front", true};
-  --{"cockpitWheelHardpoints", 0, "details1cockpitwheelshardpoints", true};
-  --{"Gear Bay", 0, "details1cockpitwheelshardpoints", true};
+  {"Gear and metal", DIFFUSE, "a4e_blueangels-bot", true};
+  {"Cockpit", DIFFUSE, "a4e_blueangels-bot", true};
+  --{"Front",	0, "a4e_ext_front", true};
+  --{"cockpitWheelHardpoints", DIFFUSE, "a4e_ext_wheels_bays", true};
+  --{"Gear Bay", DIFFUSE, "a4e_ext_wheels_bays", true};
 
   -- flaps, used on most liveries
-  {"flaps_001", 0, "a4e_bort_gold", true};
-  {"flaps_010", 0, "empty", true};
-  {"flaps_100", 0, "empty", true};
+  {"flaps_001", DIFFUSE, "a4e_bort_gold", true};
+  {"flaps_010", DIFFUSE, "empty", true};
+  {"flaps_100", DIFFUSE, "empty", true};
 
   -- three digit, smaller nose numbers, used on most liveries
-  {"nose_001", 0, "empty", true};
-  {"nose_010", 0, "empty", true};
-  {"nose_100", 0, "empty", true};
+  {"nose_001", DIFFUSE, "empty", true};
+  {"nose_010", DIFFUSE, "empty", true};
+  {"nose_100", DIFFUSE, "empty", true};
 
   -- two digit top-of-tail, used on most USMC liveries
-  {"tail_aggressor_001", 0, "empty", true};
-  {"tail_aggressor_010", 0, "empty", true};
+  {"tail_aggressor_001", DIFFUSE, "empty", true};
+  {"tail_aggressor_010", DIFFUSE, "empty", true};
 
   -- three digit top-of-rudder, used on most USN liveries
-  {"rudder_001", 0, "empty", true};
-  {"rudder_010", 0, "empty", true};
-  {"rudder_100", 0, "empty", true};
+  {"rudder_001", DIFFUSE, "empty", true};
+  {"rudder_010", DIFFUSE, "empty", true};
+  {"rudder_100", DIFFUSE, "empty", true};
 
   -- wing bort used on most USN liveries
-  {"wing_001", 0, "empty", true};
-  {"wing_010", 0, "empty", true};
-  {"wing_100", 0, "empty", true};
+  {"wing_001", DIFFUSE, "empty", true};
+  {"wing_010", DIFFUSE, "empty", true};
+  {"wing_100", DIFFUSE, "empty", true};
 
   -- 2-digit large nose numbers, used on USN/USMC aggressors
-  {"nose_aggressor_001", 0, "empty", true};
-  {"nose_aggressor_010", 0, "empty", true};
+  {"nose_aggressor_001", DIFFUSE, "empty", true};
+  {"nose_aggressor_010", DIFFUSE, "empty", true};
 
   -- 3-digit large tail numbers, used on some USN/USMC aggressors
-  {"tail_001", 0, "empty", true};
-  {"tail_010", 0, "empty", true};
-  {"tail_100", 0, "empty", true};
+  {"tail_001", DIFFUSE, "empty", true};
+  {"tail_010", DIFFUSE, "empty", true};
+  {"tail_100", DIFFUSE, "empty", true};
 
   -- custom fuel tanks: 150 gallon
-  {"fuel_150_a4e", 0, "a4e_blueangels-ft", true};
+  {"fuel_150_a4e", DIFFUSE, "a4e_blueangels-ft", true};
 
   -- custom fuel tank: 300 gallon left/right
-  {"fuel_300lr_a4e", 0, "a4e_blueangels-ft", true};
+  {"fuel_300lr_a4e", DIFFUSE, "a4e_blueangels-ft", true};
 
   -- custom fuel tank: 300 gallon center
-  {"fuel_300c_a4e", 0, "a4e_blueangels-ft", true};
+  {"fuel_300c_a4e", DIFFUSE, "a4e_blueangels-ft", true};
 }
--- A-4E-C/Liveries/A-4E-C/Trainer USN NFWS Gray/description.lua
+++ A-4E-C/Liveries/A-4E-C/Trainer USN NFWS Gray/description.lua
@@ -2,55 +2,55 @@ name = "USN Naval Fighter Weapons School - Gray"
 countries = {"USA"}
 
 livery = {
-  {"Top", 0, "nfws_gray_top", true};
-  {"Top", ROUGHNESS_METALLIC, "fus_top_v494_RoughMet", true};
-  {"Bottom", 0, "nfws_gray_bot", true};
-  {"Bottom", ROUGHNESS_METALLIC, "fus_bot_v472_RoughMet", true};
-  {"Gear and metal", 0, "nfws_gray_bot", true};
-  {"Cockpit", 0, "nfws_gray_bot", true};
-  {"Front",	0, "fus_front", true};
-  {"cockpitWheelHardpoints", 0, "details1cockpitwheelshardpoints", true};
-  {"Gear Bay", 0, "details1cockpitwheelshardpoints", true};
+  {"Top", DIFFUSE, "nfws_gray_top", true};
+  {"Top", ROUGHNESS_METALLIC, "a4e_ext_top_RoughMet", true};
+  {"Bottom", DIFFUSE, "nfws_gray_bot", true};
+  {"Bottom", ROUGHNESS_METALLIC, "a4e_ext_bottom_RoughMet", true};
+  {"Gear and metal", DIFFUSE, "nfws_gray_bot", true};
+  {"Cockpit", DIFFUSE, "nfws_gray_bot", true};
+  {"Front",	0, "a4e_ext_front", true};
+  {"cockpitWheelHardpoints", DIFFUSE, "a4e_ext_wheels_bays", true};
+  {"Gear Bay", DIFFUSE, "a4e_ext_wheels_bays", true};
 
   -- flaps, used on most liveries
-  {"flaps_001", 0, "a4e_bort_white", true};
-  {"flaps_010", 0, "a4e_bort_white", true};
-  {"flaps_100", 0, "a4e_bort_white", true};
+  {"flaps_001", DIFFUSE, "a4e_bort_white", true};
+  {"flaps_010", DIFFUSE, "a4e_bort_white", true};
+  {"flaps_100", DIFFUSE, "a4e_bort_white", true};
 
   -- three digit, smaller nose numbers, used on most liveries
-  {"nose_001", 0, "empty", true};
-  {"nose_010", 0, "empty", true};
-  {"nose_100", 0, "empty", true};
+  {"nose_001", DIFFUSE, "empty", true};
+  {"nose_010", DIFFUSE, "empty", true};
+  {"nose_100", DIFFUSE, "empty", true};
 
   -- two digit top-of-tail, used on most USMC liveries
-  {"tail_aggressor_001", 0, "empty", true};
-  {"tail_aggressor_010", 0, "empty", true};
+  {"tail_aggressor_001", DIFFUSE, "empty", true};
+  {"tail_aggressor_010", DIFFUSE, "empty", true};
 
   -- three digit top-of-rudder, used on most USN liveries
-  {"rudder_001", 0, "a4e_bort_red-gold.dds", true};
-  {"rudder_010", 0, "a4e_bort_red-gold.dds", true};
-  {"rudder_100", 0, "a4e_bort_red-gold.dds", true};
+  {"rudder_001", DIFFUSE, "a4e_bort_red-gold.dds", true};
+  {"rudder_010", DIFFUSE, "a4e_bort_red-gold.dds", true};
+  {"rudder_100", DIFFUSE, "a4e_bort_red-gold.dds", true};
 
   -- wing bort used on most USN liveries
-  {"wing_001", 0, "empty", true};
-  {"wing_010", 0, "empty", true};
-  {"wing_100", 0, "empty", true};
+  {"wing_001", DIFFUSE, "empty", true};
+  {"wing_010", DIFFUSE, "empty", true};
+  {"wing_100", DIFFUSE, "empty", true};
 
   -- 2-digit large nose numbers, used on USN/USMC aggressors
-  {"nose_aggressor_001", 0, "a4e_bort_red-gold.dds", true};
-  {"nose_aggressor_010", 0, "a4e_bort_red-gold.dds", true};
+  {"nose_aggressor_001", DIFFUSE, "a4e_bort_red-gold.dds", true};
+  {"nose_aggressor_010", DIFFUSE, "a4e_bort_red-gold.dds", true};
 
   -- 3-digit large tail numbers, used on some USN/USMC aggressors
-  {"tail_001", 0, "empty", true};
-  {"tail_010", 0, "empty", true};
-  {"tail_100", 0, "empty", true};
+  {"tail_001", DIFFUSE, "empty", true};
+  {"tail_010", DIFFUSE, "empty", true};
+  {"tail_100", DIFFUSE, "empty", true};
 
   -- custom fuel tanks: 150 gallon
-  {"fuel_150_a4e", 0, "uvMapsFuelTank_DFT150_DFT300_nfws_gray", true};
+  {"fuel_150_a4e", DIFFUSE, "uvMapsFuelTank_DFT150_DFT300_nfws_gray", true};
 
   -- custom fuel tank: 300 gallon left/right
-  {"fuel_300lr_a4e", 0, "uvMapsFuelTank_DFT150_DFT300_nfws_gray", true};
+  {"fuel_300lr_a4e", DIFFUSE, "uvMapsFuelTank_DFT150_DFT300_nfws_gray", true};
 
   -- custom fuel tank: 300 gallon center
-  {"fuel_300c_a4e", 0, "uvMapsFuelTank_DFT150_DFT300_nfws_gray", true};
+  {"fuel_300c_a4e", DIFFUSE, "uvMapsFuelTank_DFT150_DFT300_nfws_gray", true};
 }
