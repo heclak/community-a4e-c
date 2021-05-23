0d21b5dcaedcb54f322c7cd98cb85a3e55eaba1f
Partario Flynn <partario.flynn@gmail.com>
2021-05-23 18:06:44 -0400

    added wheelbrake indicators to controls indicator

--- A-4E-C/Cockpit/Scripts/ControlsIndicator/ControlsIndicator_page.lua
+++ A-4E-C/Cockpit/Scripts/ControlsIndicator/ControlsIndicator_page.lua
@@ -5,7 +5,6 @@ dofile(LockOn_Options.script_path.."EFM_Data_Bus.lua")
 local draw_background           = MakeMaterial("arcade.tga", {32, 96, 128, 128})
 local draw_axis                 = MakeMaterial("arcade.tga", {0, 16, 48, 168})
 local draw_input                = MakeMaterial("arcade.tga", {255, 192, 0, 255})
-local draw_indicator            = MakeMaterial("arcade.tga", {0, 0, 255, 255})
 
 SetCustomScale(1.0)
 
@@ -33,10 +32,10 @@ base.name		                = "base"
 base.primitivetype              = "triangles"
 base.material                   = draw_background
 base.vertices                   = {
-                                    {-(sizeX + throttle_shift + rud_shift + 3.0 * line_width + ds), -(sizeY)},
+                                    {-(sizeX + throttle_shift + rud_shift + 3.0 * line_width + ds), -sizeY},
                                     {-(sizeX + throttle_shift + rud_shift + 3.0 * line_width + ds), sizeY}, 
                                     { sizeX + ds, sizeY},
-                                    { sizeX + ds, -(sizeY)}
+                                    { sizeX + ds, -sizeY}
                                   }        
 base.indices                    = default_box_indices
 base.init_pos                   = {(-1 * aspect + 1.5 * sizeX),(1 - 1.3 * sizeX)}
@@ -106,10 +105,12 @@ AddElement(rudder_scale)
 
 -- draw RUDDER position
 rudder_index                    = Copy(roll_scale)
-rudder_index.vertices           = {{-2.0 * line_width, -line_width},
+rudder_index.vertices           = {
+                                    {-2.0 * line_width, -line_width},
                                     {-2.0 * line_width, line_width},
                                     {2.0 * line_width, line_width},
-                                    {2.0 * line_width, -line_width}}
+                                    {2.0 * line_width, -line_width}
+                                  }
 rudder_index.element_params     = {"RUDDER_PEDALS"}  
 rudder_index.controllers 	    = {{"move_up_down_using_parameter", 0, -sizeX}}
 rudder_index.init_rot           = {90, 0}
@@ -131,14 +132,70 @@ AddElement(throttle_scale)
 
 -- draw THROTTLE position
 throttle_index                  = Copy(roll_scale)
-throttle_index.vertices         = {{-2.0 * line_width, -line_width},
+throttle_index.vertices         = {
+                                    {-2.0 * line_width, -line_width},
                                     {-2.0 * line_width, line_width},
                                     { 2.0 * line_width, line_width},
-                                    { 2.0 * line_width, -line_width}}
+                                    { 2.0 * line_width, -line_width}
+                                  }
 throttle_index.element_params   = {"FM_THROTTLE_POSITION"}  
-throttle_index.controllers      = {{"move_up_down_using_parameter",0, 2.0 * sizeX}}
+throttle_index.controllers      = {{"move_up_down_using_parameter", 0, 2.0 * sizeX}}
 throttle_index.tex_params	    = {256 / 512, 176.5 / 512, 0.5 * tex_scale / 3, 2 * tex_scale / 3}
 throttle_index.init_rot         = {-90, 0, 0}
 throttle_index.material         = draw_input
 throttle_index.parent_element   = throttle_scale.name
 AddElement(throttle_index)
+
+-- scale WHEELBRAKE LEFT position
+wbrakel_scale                = Copy(pitch_scale)
+wbrakel_scale.vertices       = {
+                                    {0, -line_width},
+                                    {0, line_width},
+                                    {sizeX - 8.0 * ds, line_width},
+                                    {sizeX - 8.0 * ds, -line_width}
+                                  }
+wbrakel_scale.init_pos       = {-sizeX + 2.0 * ds, -sizeX}
+AddElement(wbrakel_scale)
+
+-- draw WHEELBRAKE LEFT position
+wbrakel_index                = Copy(roll_scale)
+wbrakel_index.vertices       = {
+                                    {-2.0 * line_width, -line_width},
+                                    {-2.0 * line_width, line_width},
+                                    { 2.0 * line_width, line_width},
+                                    { 2.0 * line_width, -line_width}
+                                  }
+wbrakel_index.element_params = {"LEFT_BRAKE_PEDAL"}  
+wbrakel_index.controllers    = {{"move_up_down_using_parameter", 0, sizeX - 8.0 * ds}}
+wbrakel_index.tex_params	    = {256 / 512, 176.5 / 512, 0.5 * tex_scale / 3, 2 * tex_scale / 3}
+wbrakel_index.init_rot         = {-90, 0, 0}
+wbrakel_index.material         = draw_input
+wbrakel_index.parent_element   = wbrakel_scale.name
+AddElement(wbrakel_index)
+
+-- scale WHEELBRAKE RIGHT position
+wbraker_scale                = Copy(pitch_scale)
+wbraker_scale.vertices       = {
+                                    {0, -line_width},
+                                    {0, line_width},
+                                    {sizeX - 8.0 * ds, line_width},
+                                    {sizeX - 8.0 * ds, -line_width}
+                                  }
+wbraker_scale.init_pos       = {sizeX - 2.0 * ds, -sizeX}
+AddElement(wbraker_scale)
+
+-- draw WHEELBRAKE RIGHT position
+wbraker_index                = Copy(roll_scale)
+wbraker_index.vertices       = {
+                                    {-2.0 * line_width, -line_width},
+                                    {-2.0 * line_width, line_width},
+                                    { 2.0 * line_width, line_width},
+                                    { 2.0 * line_width, -line_width}
+                                  }
+wbraker_index.element_params = {"RIGHT_BRAKE_PEDAL"}  
+wbraker_index.controllers    = {{"move_up_down_using_parameter", 0, sizeX - 8.0 * ds}}
+wbraker_index.tex_params	    = {256 / 512, 176.5 / 512, 0.5 * tex_scale / 3, 2 * tex_scale / 3}
+wbraker_index.init_rot         = {-90, 0, 0}
+wbraker_index.material         = draw_input
+wbraker_index.parent_element   = wbraker_scale.name
+AddElement(wbraker_index)
