5c2772fe4f146a892b91cbf19f2a41f819f9f2b6
Partario Flynn <partario.flynn@gmail.com>
2021-05-27 14:08:54 -0400

    added trackir axes to joystick inputs
    this is useful for creating an axis-look for those who like them (elite: dangerous style). to do so, the user should bind the TrackIR Yaw (X) and Pitch (Y).

--- A-4E-C/Input/joystick/default.lua
+++ A-4E-C/Input/joystick/default.lua
@@ -566,6 +566,15 @@ join(res.axisCommands,{
     {action = device_commands.left_wheelbrake_AXIS,  name = _('Wheel Brake Left'),  category = {_('Systems')}},
     {action = device_commands.right_wheelbrake_AXIS,  name = _('Wheel Brake Right'), category = {_('Systems')}},
     
+
+    {combos = {{key = 'TRACKIR_PITCH'}},	action = iHeadTrackerPitchNormed	, name = _('Head Tracker : Pitch')},
+    {combos = {{key = 'TRACKIR_YAW'}},  	action = iHeadTrackerYawNormed		, name = _('Head Tracker : Yaw')},
+    {combos = {{key = 'TRACKIR_ROLL'}}, 	action = iHeadTrackerRollNormed		, name = _('Head Tracker : Roll')},
+    
+    {combos = {{key = 'TRACKIR_X'}}, 		action = iHeadTrackerPosZNormed		, name = _('Head Tracker : Right/Left')},
+    {combos = {{key = 'TRACKIR_Y'}}, 		action = iHeadTrackerPosYNormed		, name = _('Head Tracker : Up/Down')},
+    {combos = {{key = 'TRACKIR_Z'}}, 		action = iHeadTrackerPosXNormed		, name = _('Head Tracker : Forward/Backward')},
+    
 	--Old throttle
     --{combos = defaultDeviceAssignmentFor("rudder"),  action = iCommandPlaneRudder	   , name = _('Rudder')},
 	--{combos = defaultDeviceAssignmentFor("thrust"),  action = iCommandPlaneThrustCommon, name = _('Thrust')},
