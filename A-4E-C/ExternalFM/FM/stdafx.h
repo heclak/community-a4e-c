2d9537366dbaaf669a49b463e285b394bab37c6e
Joshua Nelson <joshnel123@gmail.com>
2021-05-28 23:52:37 +0100

    Update version for 2.0.0-beta-5

--- A-4E-C/ExternalFM/FM/stdafx.h
+++ A-4E-C/ExternalFM/FM/stdafx.h
@@ -11,7 +11,7 @@
 // Windows Header Files:
 #include <windows.h>
 
-static const char srcvers[] = "A-4E-C Version 2.0.0-Beta-4";
+static const char srcvers[] = "A-4E-C Version 2.0.0-Beta-5";
 
 #endif
 // TODO: reference additional headers your program requires here
