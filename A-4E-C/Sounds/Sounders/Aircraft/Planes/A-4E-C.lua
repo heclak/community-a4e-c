75982a1f2e256c107f452b5c46665386bf1fcd14
Joshua Nelson <joshnel123@gmail.com>
2021-05-30 00:15:57 +0100

    Renamed Sounder Files

--- A-4E-C/Sounds/Sounders/Aircraft/Planes/A-4E-C.lua
+++ A-4E-C/Sounds/Sounders/Aircraft/Planes/A-4E-C.lua
@@ -1,9 +1,9 @@
-dofile("Aircraft/Planes/Plane.lua")
+dofile("Aircraft/Planes/a4_Plane.lua")
 
 A4EC = plane:new()
 
-dofile("Aircraft/Engines/Engine.lua")
-dofile("Aircraft/Engines/APUs/BasicAPU.lua")
+dofile("Aircraft/Engines/J52P8.lua")
+--dofile("Aircraft/Engines/APUs/BasicAPU.lua")
 
 function A4EC:createEngines()
 	self.engines[1] = engine:new()
