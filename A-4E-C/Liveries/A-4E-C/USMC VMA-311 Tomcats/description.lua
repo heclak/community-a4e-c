5bb8907abdced8e255a16a1d28be08ab0b80e404
Partario Flynn <partario.flynn@gmail.com>
2021-06-02 07:43:54 -0400

    fixed missing nose numbers on tomcats livery

--- A-4E-C/Liveries/A-4E-C/USMC VMA-311 Tomcats/description.lua
+++ A-4E-C/Liveries/A-4E-C/USMC VMA-311 Tomcats/description.lua
@@ -19,8 +19,8 @@ livery = {
   {"flaps_100", DIFFUSE, "a4e_bort_white", true};
 
   -- three digit, smaller nose numbers, used on most liveries
-  {"nose_001", DIFFUSE, "empty", true};
-  {"nose_010", DIFFUSE, "empty", true};
+  {"nose_001", DIFFUSE, "a4e_bort_black", true};
+  {"nose_010", DIFFUSE, "a4e_bort_black", true};
   {"nose_100", DIFFUSE, "empty", true};
 
   -- two digit top-of-tail, used on most USMC liveries
