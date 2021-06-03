740eb896ddafc9d1317bad73e65e431a5d7670b1
Partario Flynn <partario.flynn@gmail.com>
2021-06-03 17:09:23 -0400

    updated liveries with smaller 2-digit modex placements
    added two new bort sets -- due to placement, these only accomodate 2-digit placement, but you can use the hundreds and 10s to attain a third new placement option for the set.
    - black small taped numbers (1s and 10s digits)
    - black small smooth numbers (1s and 10s digits)
    (files added to binary repo)

--- A-4E-C/Liveries/A-4E-C/International Argentina/description.lua
+++ A-4E-C/Liveries/A-4E-C/International Argentina/description.lua
@@ -20,7 +20,7 @@ livery = {
 
     -- three digit, smaller nose numbers, used on most liveries
     {"nose_001", DIFFUSE, "a4e_bort_black", true};
-    {"nose_010", DIFFUSE, "a4e_bort_black", true};
+    {"nose_010", DIFFUSE, "empty", true};
     {"nose_100", DIFFUSE, "empty", true};
 
     -- two digit top-of-tail, used on most USMC liveries
@@ -38,8 +38,8 @@ livery = {
     {"wing_100", DIFFUSE, "empty", true};
 
     -- 2-digit large nose numbers, used on USN/USMC aggressors
-    {"nose_aggressor_001", DIFFUSE, "empty", true};
-    {"nose_aggressor_010", DIFFUSE, "empty", true};
+    {"nose_aggressor_001", DIFFUSE, "a4e_bort_black_small_1s", true};
+    {"nose_aggressor_010", DIFFUSE, "a4e_bort_black_small_10s", true};
 
     -- 3-digit large tail numbers, used on some USN/USMC aggressors
     {"tail_001", DIFFUSE, "empty", true};
