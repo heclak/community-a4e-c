d117c4034966ffdc02fd8b038cb79490f08efb8a
Partario Flynn <partario.flynn@gmail.com>
2021-06-07 19:00:37 -0400

    updated radar colors and brightnesses

--- A-4E-C/Cockpit/Scripts/materials.lua
+++ A-4E-C/Cockpit/Scripts/materials.lua
@@ -8,10 +8,10 @@ materials["DBG_BLACK"]   = {0, 0, 0, 100}
 materials["DBG_GREEN"]   = {0, 80, 0, 255}
 materials["DBG_RED"]     = {255, 0, 0, 100}
 materials["DBG_CLEAR"]   = {0, 0, 0, 0}
-materials["BLOB_COLOR"] = {0, 128, 0, 192}
+materials["BLOB_COLOR"] = {0, 255, 192, 224}
 materials["RADAR_FOV"] 	= {17, 80, 7, 0}
-materials["RADAR_GRID"] = {0, 100, 0, 192}
-materials["RADAR_SCRIBE"] = {0, 200,0, 192}
+materials["RADAR_GRID"] = {0, 96, 48, 192}
+materials["RADAR_SCRIBE"] = {0, 192, 129, 176}
 materials["GUNSIGHT_GLASS"] = {0, 120, 0, 128}
 materials["GUNSIGHT_COLOR"] = {255, 48, 0, 255}
 
