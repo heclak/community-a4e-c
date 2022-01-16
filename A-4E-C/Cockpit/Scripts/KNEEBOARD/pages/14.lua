906efa29d86ce451f37fa2cb1692432254d8098e
Partario Flynn <partario.flynn@gmail.com>
2022-01-16 10:22:49 -0500

    set kneeboard pagination
    artwork on binaries will follow shortly

--- A-4E-C/Cockpit/Scripts/KNEEBOARD/pages/14.lua
+++ A-4E-C/Cockpit/Scripts/KNEEBOARD/pages/14.lua
@@ -3,7 +3,7 @@ dofile(LockOn_Options.common_script_path.."KNEEBOARD/indicator/definitions.lua")
 SetScale(FOV)
 
 -- add background image for kneeboard
-add_picture(LockOn_Options.script_path.."../KneeboardResources/a4e_cockpit_kneeboard_15.png")
+add_picture(LockOn_Options.script_path.."../KneeboardResources/a4e_cockpit_kneeboard_14.png")
 
 
 local gettext = require("i_18n")
