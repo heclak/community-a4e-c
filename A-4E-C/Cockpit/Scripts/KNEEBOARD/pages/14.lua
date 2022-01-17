3905057112ba2ae853f4658465d59363933c2542
Partario Flynn <partario.flynn@gmail.com>
2022-01-16 19:03:53 -0500

    kneeboard mission loadout page
    - added fuel stores to mission loadout page (1-4)
    - more complete stores_config naming lookup table
    - added gamepad storage
    - updated binaries with new kneeboard pages and refined layout accordingly

--- A-4E-C/Cockpit/Scripts/KNEEBOARD/pages/14.lua
+++ A-4E-C/Cockpit/Scripts/KNEEBOARD/pages/14.lua
@@ -1,10 +1,8 @@
---dofile(LockOn_Options.script_path.."Systems/weapon_system.lua")
 dofile(LockOn_Options.common_script_path.."KNEEBOARD/indicator/definitions.lua")
 SetScale(FOV)
 
 -- add background image for kneeboard
 add_picture(LockOn_Options.script_path.."../KneeboardResources/a4e_cockpit_kneeboard_14.png")
-
 
 local gettext = require("i_18n")
 _ = gettext.translate
@@ -27,8 +25,8 @@ local FirstLineY	= 1.4
 local LineSizeY		= 0.1
 
 --define basic page margins to left and right
-local margin_xl = -0.8
-local margin_xr = 0.75
+local margin_xl = -0.9
+local margin_xr = 0.735
 
 --define the first line to appear
 local function getLineY(line)
@@ -37,61 +35,140 @@ end
 
 --define blank lines at top of page for station diagram
 --define each section by line count offset from prior section
-local offset_line_loadout = 10.5
-local offset_line_cbus = offset_line_loadout + 6.5
-local offset_line_cms = offset_line_cbus + 5.5
+--to reorder these sections, end each with a value of 1.5 higher than the prior section's total number of lines.
+local offset_line_fuel          = 5
+local offset_line_loadout       = offset_line_fuel + 3.5
+local offset_line_programmer    = offset_line_loadout + 6.5
+local offset_line_cbus          = offset_line_programmer + 4
+local offset_line_cms           = offset_line_cbus + 4.5
 
 --define columned placements
-local offset_loadout        = margin_xl + 0.15
-local offset_cbu_quantity   = margin_xl + 0.55
-local offset_cbu_method     = offset_cbu_quantity + 0.1
-local offset_cms_quantity   = margin_xl + 0.55
-local offset_cms_method     = offset_cms_quantity + 0.1
+local offset_indent_line        = margin_xl + 0.025
+local offset_fuel_quantity      = offset_indent_line + 0.25
+local offset_fuel_lbs           = offset_fuel_quantity + 0.035
+local offset_loadout_name       = offset_indent_line + 0.075
+local offset_cbu_quantity       = offset_indent_line + 0.55
+local offset_cbu_method         = offset_cbu_quantity + 0.1
+local offset_cms_quantity       = offset_cbu_quantity
+local offset_cms_method         = offset_cbu_method
+
+------------
+--  FUEL  --
+------------
+--fuel title
+txt_HeaderFuel = CreateElement "ceStringPoly"
+txt_HeaderFuel.name = "txt_HeaderFuel"
+txt_HeaderFuel.material = "font_kneeboard"
+txt_HeaderFuel.init_pos = {margin_xl, getLineY(offset_line_fuel), 0}
+txt_HeaderFuel.value = _("FUEL STORES")
+txt_HeaderFuel.alignment = "LeftBottom"
+txt_HeaderFuel.stringdefs = predefined_font_title
+AddElement(txt_HeaderFuel)
+
+--internal fuel
+txt_FuelLabelInternal                   = CreateElement "ceStringPoly"
+txt_FuelLabelInternal.name              = "txt_FuelLabelInternal"
+txt_FuelLabelInternal.material          = "font_kneeboard"
+txt_FuelLabelInternal.init_pos          = {offset_indent_line, getLineY(offset_line_fuel + 1), 0}
+txt_FuelLabelInternal.value             = _("INT:")
+txt_FuelLabelInternal.alignment         = "LeftBottom"
+txt_FuelLabelInternal.stringdefs        = predefined_font_item
+AddElement(txt_FuelLabelInternal)
+
+txt_FuelLabelExternal				    = CreateElement "ceStringPoly"
+txt_FuelLabelExternal.name			    = "txt_FuelLabelExternal"
+txt_FuelLabelExternal.material		    = "font_kneeboard"
+txt_FuelLabelExternal.init_pos		    = {offset_fuel_quantity,  getLineY(offset_line_fuel + 1), 0}
+txt_FuelLabelExternal.formats		    = {"%.0f"}
+txt_FuelLabelExternal.element_params    = {"kneeboard_fuel_int"}
+txt_FuelLabelExternal.controllers       = {{"text_using_parameter",0,0}} --first index is for element_params (starting with 0) , second for formats ( starting with 0)
+txt_FuelLabelExternal.alignment		    = "RightBottom"
+txt_FuelLabelExternal.stringdefs		= predefined_font_item
+AddElement(txt_FuelLabelExternal)
+
+txt_Fuel_Lbs_Internal                   = CreateElement "ceStringPoly"
+txt_Fuel_Lbs_Internal.name              = "txt_Fuel_Lbs_Internal"
+txt_Fuel_Lbs_Internal.material          = "font_kneeboard"
+txt_Fuel_Lbs_Internal.init_pos          = {offset_fuel_lbs, getLineY(offset_line_fuel + 1), 0}
+txt_Fuel_Lbs_Internal.value             = _("LBS.")
+txt_Fuel_Lbs_Internal.alignment         = "LeftBottom"
+txt_Fuel_Lbs_Internal.stringdefs        = predefined_font_item
+AddElement(txt_Fuel_Lbs_Internal)
+
+--external fuel
+txt_FuelLabelExternal                   = CreateElement "ceStringPoly"
+txt_FuelLabelExternal.name              = "txt_FuelLabelExternal"
+txt_FuelLabelExternal.material          = "font_kneeboard"
+txt_FuelLabelExternal.init_pos          = {offset_indent_line, getLineY(offset_line_fuel + 2), 0}
+txt_FuelLabelExternal.value             = _("EXT:")
+txt_FuelLabelExternal.alignment         = "LeftBottom"
+txt_FuelLabelExternal.stringdefs        = predefined_font_item
+AddElement(txt_FuelLabelExternal)
+
+txt_FuelWeightExternal				    = CreateElement "ceStringPoly"
+txt_FuelWeightExternal.name			    = "txt_FuelWeightExternal"
+txt_FuelWeightExternal.material		    = "font_kneeboard"
+txt_FuelWeightExternal.init_pos		    = {offset_fuel_quantity,  getLineY(offset_line_fuel + 2), 0}
+txt_FuelWeightExternal.formats		    = {"%.0f"}
+txt_FuelWeightExternal.element_params   = {"kneeboard_fuel_ext"}
+txt_FuelWeightExternal.controllers      = {{"text_using_parameter",0,0}} --first index is for element_params (starting with 0) , second for formats ( starting with 0)
+txt_FuelWeightExternal.alignment		= "RightBottom"
+txt_FuelWeightExternal.stringdefs		= predefined_font_item
+AddElement(txt_FuelWeightExternal)
+
+txt_FuelLbsExternal                     = CreateElement "ceStringPoly"
+txt_FuelLbsExternal.name                = "txt_FuelLbsExternal"
+txt_FuelLbsExternal.material            = "font_kneeboard"
+txt_FuelLbsExternal.init_pos            = {offset_fuel_lbs, getLineY(offset_line_fuel + 2), 0}
+txt_FuelLbsExternal.value               = _("LBS.")
+txt_FuelLbsExternal.alignment           = "LeftBottom"
+txt_FuelLbsExternal.stringdefs          = predefined_font_item
+AddElement(txt_FuelLbsExternal)
 
 ------------------------
 --  STATION LOADOUTS  --
 ------------------------
 --station loadout title
-Name_InitLoad = CreateElement "ceStringPoly"
-Name_InitLoad.name = "Name_InitLoad"
-Name_InitLoad.material = "font_kneeboard"
-Name_InitLoad.init_pos = {margin_xl, getLineY(offset_line_loadout), 0}
-Name_InitLoad.value = _("INITIAL LOADOUT")
-Name_InitLoad.alignment = "LeftBottom"
-Name_InitLoad.stringdefs = predefined_font_title
-AddElement(Name_InitLoad)
+txt_HeaderLoadout                          = CreateElement "ceStringPoly"
+txt_HeaderLoadout.name                     = "txt_HeaderLoadout"
+txt_HeaderLoadout.material                 = "font_kneeboard"
+txt_HeaderLoadout.init_pos                 = {margin_xl, getLineY(offset_line_loadout), 0}
+txt_HeaderLoadout.value                    = _("STATION LOADOUTS")
+txt_HeaderLoadout.alignment                = "LeftBottom"
+txt_HeaderLoadout.stringdefs               = predefined_font_title
+AddElement(txt_HeaderLoadout)
 
-Header_Quantity = CreateElement "ceStringPoly"
-Header_Quantity.name = "Header_Quantity"
-Header_Quantity.material = "font_kneeboard"
-Header_Quantity.init_pos = {margin_xr, getLineY(offset_line_loadout), 0}
-Header_Quantity.value = _("QTY")
-Header_Quantity.alignment = "RightBottom"
-Header_Quantity.stringdefs = predefined_font_header
-AddElement(Header_Quantity)
+txt_HeaderLoadout_Quantity = CreateElement "ceStringPoly"
+txt_HeaderLoadout_Quantity.name = "txt_HeaderLoadout_Quantity"
+txt_HeaderLoadout_Quantity.material = "font_kneeboard"
+txt_HeaderLoadout_Quantity.init_pos = {margin_xr, getLineY(offset_line_loadout), 0}
+txt_HeaderLoadout_Quantity.value = _("QTY")
+txt_HeaderLoadout_Quantity.alignment = "RightBottom"
+txt_HeaderLoadout_Quantity.stringdefs = predefined_font_header
+AddElement(txt_HeaderLoadout_Quantity)
 
 --station loadout items
 for station = 1,5 do
     local item_pos_y = getLineY(offset_line_loadout + station)
 
-    EnumeratedStation                     = CreateElement "ceStringPoly"
-    EnumeratedStation.name                = "EnumeratedStation"..station
-    EnumeratedStation.material            = "font_kneeboard"
-    EnumeratedStation.init_pos            = {margin_xl, item_pos_y, 0}
-    EnumeratedStation.alignment           = "LeftBottom"
-    EnumeratedStation.stringdefs          = predefined_font_item
-    EnumeratedStation.value               = tostring(station)..". /"
+    EnumeratedStation                   = CreateElement "ceStringPoly"
+    EnumeratedStation.name              = "EnumeratedStation"..station
+    EnumeratedStation.material          = "font_kneeboard_hint"
+    EnumeratedStation.init_pos          = {offset_indent_line, item_pos_y, 0}
+    EnumeratedStation.alignment         = "LeftBottom"
+    EnumeratedStation.stringdefs        = predefined_font_item
+    EnumeratedStation.value             = tostring(station).."."
     AddElement(EnumeratedStation)
 
-    LoudoutByStation                     = CreateElement "ceStringPoly"
-    LoudoutByStation.name                = "LoudoutByStation"..station
-    LoudoutByStation.material            = "font_kneeboard"
-    LoudoutByStation.init_pos            = {offset_loadout, item_pos_y, 0}
-    LoudoutByStation.alignment           = "LeftBottom"
-    LoudoutByStation.stringdefs          = predefined_font_item
-    LoudoutByStation.formats              = {"%s"}
-    LoudoutByStation.element_params      = {"loadout_station"..station}
-    LoudoutByStation.controllers         = {{"text_using_parameter",0,0}} --first index is for element_params (starting with 0) , second for formats ( starting with 0)
+    LoudoutByStation                    = CreateElement "ceStringPoly"
+    LoudoutByStation.name               = "LoudoutByStation"..station
+    LoudoutByStation.material           = "font_kneeboard"
+    LoudoutByStation.init_pos           = {offset_loadout_name, item_pos_y, 0}
+    LoudoutByStation.alignment          = "LeftBottom"
+    LoudoutByStation.stringdefs         = predefined_font_item
+    LoudoutByStation.formats            = {"%s"}
+    LoudoutByStation.element_params     = {"loadout_station"..station}
+    LoudoutByStation.controllers        = {{"text_using_parameter",0,0}} --first index is for element_params (starting with 0) , second for formats ( starting with 0)
     AddElement(LoudoutByStation)
 
     LoadoutQuantity                     = CreateElement "ceStringPoly"
@@ -100,7 +177,7 @@ for station = 1,5 do
     LoadoutQuantity.init_pos            = {margin_xr, item_pos_y, 0}
     LoadoutQuantity.alignment           = "RightBottom"
     LoadoutQuantity.stringdefs          = predefined_font_item
-    LoadoutQuantity.formats              = {"%.0f"}
+    LoadoutQuantity.formats             = {"%.0f"}
     LoadoutQuantity.element_params      = {"loadout_quantity"..station}
     LoadoutQuantity.controllers         = {{"text_using_parameter",0,0}} --first index is for element_params (starting with 0) , second for formats ( starting with 0)
     AddElement(LoadoutQuantity)
@@ -112,7 +189,7 @@ end
 txt_BoardTitle				= CreateElement "ceStringPoly"
 txt_BoardTitle.name			= "txt_BoardTitle"
 txt_BoardTitle.material		= "font_kneeboard"
-txt_BoardTitle.init_pos		= {margin_xl, getLineY(offset_line_cbus), 0}
+txt_BoardTitle.init_pos		= {margin_xl, getLineY(offset_line_programmer), 0}
 txt_BoardTitle.value		= "AN/ALE-29A PROGRAMMER"
 txt_BoardTitle.alignment	= "LeftBottom"
 txt_BoardTitle.stringdefs	= predefined_font_title
@@ -121,22 +198,22 @@ AddElement(txt_BoardTitle)
 ----------------------
 -- CBU CONFIGURATION
 ----------------------
-txt_WeaponsTitle				= CreateElement "ceStringPoly"
-txt_WeaponsTitle.name			= "txt_WeaponsTitle"
-txt_WeaponsTitle.material		= "font_kneeboard"
-txt_WeaponsTitle.init_pos		= {margin_xl, getLineY(offset_line_cbus + 1), 0}
+txt_WeaponsTitle			= CreateElement "ceStringPoly"
+txt_WeaponsTitle.name		= "txt_WeaponsTitle"
+txt_WeaponsTitle.material	= "font_kneeboard"
+txt_WeaponsTitle.init_pos	= {margin_xl, getLineY(offset_line_cbus), 0}
 txt_WeaponsTitle.value		= "CBU CONFIGURATION"
 txt_WeaponsTitle.alignment	= "LeftBottom"
 txt_WeaponsTitle.stringdefs	= predefined_font_header
 AddElement(txt_WeaponsTitle)
 
 --CBU-1/A
-CBU1A_LineY = getLineY(offset_line_cbus + 2)
+CBU1A_LineY = getLineY(offset_line_cbus + 1)
 
 Name_CBU1A				    = CreateElement "ceStringPoly"
 Name_CBU1A.name			    = "Name_CBU1A"
 Name_CBU1A.material		    = "font_kneeboard"
-Name_CBU1A.init_pos		    = {margin_xl,  CBU1A_LineY, 0}
+Name_CBU1A.init_pos		    = {offset_indent_line,  CBU1A_LineY, 0}
 Name_CBU1A.value		    = _("CBU-1/A")
 Name_CBU1A.alignment	    = "LeftBottom"
 Name_CBU1A.stringdefs	    = predefined_font_item
@@ -174,12 +251,12 @@ AddElement(Units_CBU1A)
 -- AddElement(Hints_CBU1A)
 
 --CBU-2/A
-CBU2A_LineY = getLineY(offset_line_cbus + 3)
+CBU2A_LineY = getLineY(offset_line_cbus + 2)
 
 Name_CBU2A				    = CreateElement "ceStringPoly"
 Name_CBU2A.name			    = "Name_CBU2A"
 Name_CBU2A.material		    = "font_kneeboard"
-Name_CBU2A.init_pos		    = {margin_xl,  CBU2A_LineY, 0}
+Name_CBU2A.init_pos		    = {offset_indent_line,  CBU2A_LineY, 0}
 Name_CBU2A.value			= _("CBU-2/A")
 Name_CBU2A.alignment		= "LeftBottom"
 Name_CBU2A.stringdefs		= predefined_font_item
@@ -215,12 +292,12 @@ Hints_CBU2A.stringdefs		= predefined_font_item
 AddElement(Hints_CBU2A)
 
 --CBU-2B/A
-CBU2BA_LineY = getLineY(offset_line_cbus + 4)
+CBU2BA_LineY = getLineY(offset_line_cbus + 3)
 
 Name_CBU2BA				    = CreateElement "ceStringPoly"
 Name_CBU2BA.name			= "Name_CBU2BA"
 Name_CBU2BA.material		= "font_kneeboard"
-Name_CBU2BA.init_pos		= {margin_xl,  CBU2BA_LineY, 0}
+Name_CBU2BA.init_pos		= {offset_indent_line,  CBU2BA_LineY, 0}
 Name_CBU2BA.value			= _("CBU-2B/A")
 Name_CBU2BA.alignment		= "LeftBottom"
 Name_CBU2BA.stringdefs		= predefined_font_item
@@ -273,7 +350,7 @@ BURSTS_LineY = getLineY(offset_line_cms + 1)
 Name_BURSTS				    = CreateElement "ceStringPoly"
 Name_BURSTS.name			= "Name_BURSTS"
 Name_BURSTS.material		= "font_kneeboard"
-Name_BURSTS.init_pos		= {margin_xl, BURSTS_LineY, 0}
+Name_BURSTS.init_pos		= {offset_indent_line, BURSTS_LineY, 0}
 Name_BURSTS.value			= _("BURST")
 Name_BURSTS.alignment		= "LeftBottom"
 Name_BURSTS.stringdefs		= predefined_font_item
@@ -314,7 +391,7 @@ BURST_INTERVAL_LineY = getLineY(offset_line_cms + 2)
 Name_BURST_INTERVAL				    = CreateElement "ceStringPoly"
 Name_BURST_INTERVAL.name			= "Name_BURST_INTERVAL"
 Name_BURST_INTERVAL.material		= "font_kneeboard"
-Name_BURST_INTERVAL.init_pos		= {margin_xl, BURST_INTERVAL_LineY, 0}
+Name_BURST_INTERVAL.init_pos		= {offset_indent_line, BURST_INTERVAL_LineY, 0}
 Name_BURST_INTERVAL.value			= _("BURST INTERVAL")
 Name_BURST_INTERVAL.alignment		= "LeftBottom"
 Name_BURST_INTERVAL.stringdefs		= predefined_font_item
@@ -355,7 +432,7 @@ SALVOS_LineY = getLineY(offset_line_cms + 3)
 Name_SALVOS				    = CreateElement "ceStringPoly"
 Name_SALVOS.name			= "Name_SALVOS"
 Name_SALVOS.material		= "font_kneeboard"
-Name_SALVOS.init_pos		= {margin_xl, SALVOS_LineY, 0}
+Name_SALVOS.init_pos		= {offset_indent_line, SALVOS_LineY, 0}
 Name_SALVOS.value			= _("SALVOS")
 Name_SALVOS.alignment		= "LeftBottom"
 Name_SALVOS.stringdefs		= predefined_font_item
@@ -396,7 +473,7 @@ SALVO_INTERVAL_LineY = getLineY(offset_line_cms + 4)
 Name_SALVO_INTERVAL				    = CreateElement "ceStringPoly"
 Name_SALVO_INTERVAL.name			= "Name_SALVO_INTERVAL"
 Name_SALVO_INTERVAL.material		= "font_kneeboard"
-Name_SALVO_INTERVAL.init_pos		= {margin_xl,  SALVO_INTERVAL_LineY, 0}
+Name_SALVO_INTERVAL.init_pos		= {offset_indent_line,  SALVO_INTERVAL_LineY, 0}
 Name_SALVO_INTERVAL.value			= _("SALVO INTERVAL")
 Name_SALVO_INTERVAL.alignment		= "LeftBottom"
 Name_SALVO_INTERVAL.stringdefs		= predefined_font_item
