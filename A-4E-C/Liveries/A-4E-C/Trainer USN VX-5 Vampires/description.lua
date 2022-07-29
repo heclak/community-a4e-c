935957e3476296b5b5bd46078699b0e1f154c734
Partario Flynn <partario.flynn@gmail.com>
2022-07-29 14:07:54 -0400

    added USN VX-5 and Community V liveries
    (binaries forthcoming, just getting things into position for final editing)

---
+++ A-4E-C/Liveries/A-4E-C/Trainer USN VX-5 Vampires/description.lua
@@ -0,0 +1,49 @@
+name = "USN VX-5 Vampires, 1986 (Fictional)"
+countries = {"USA"}
+
+livery =
+{
+  {"Top", DIFFUSE, "a4e_trn_usn_vx5_top", true};
+  {"Top", ROUGHNESS_METALLIC, "a4e_ext_top_RoughMet", true};
+  {"Bottom", DIFFUSE, "a4e_trn_usn_vx5_bot", true};
+  {"Bottom", ROUGHNESS_METALLIC, "a4e_ext_bottom_RoughMet", true};
+  {"Gear and metal", DIFFUSE, "a4e_trn_usn_vx5_bot", true};
+  {"Cockpit", DIFFUSE, "a4e_trn_usn_vx5_bot", true};
+  --{"Front",	0, "a4e_ext_front", true};
+  --{"cockpitWheelHardpoints", DIFFUSE, "a4e_ext_wheels_bays", true};
+  --{"Gear Bay", DIFFUSE, "a4e_ext_wheels_bays", true};
+  {"Pilot",	DIFFUSE, "a4e_pilot_usn", true};
+
+  -- flaps, used on most liveries
+  {"flaps_001", DIFFUSE, "a4e_bort_grey", true};
+  {"flaps_010", DIFFUSE, "a4e_bort_grey", true};
+  {"flaps_100", DIFFUSE, "empty", true};
+
+  -- three digit, smaller nose numbers, used on most liveries
+  {"nose_001", DIFFUSE, "empty", true};
+  {"nose_010", DIFFUSE, "empty", true};
+  {"nose_100", DIFFUSE, "empty", true};
+
+  -- two digit top-of-tail, used on most USMC liveries
+  {"tail_aggressor_001", DIFFUSE, "a4e_bort_grey", true};
+  {"tail_aggressor_010", DIFFUSE, "a4e_bort_grey", true};
+
+  -- three digit top-of-rudder, used on most USN liveries
+  {"rudder_001", DIFFUSE, "empty", true};
+  {"rudder_010", DIFFUSE, "empty", true};
+  {"rudder_100", DIFFUSE, "empty", true};
+
+  -- wing bort used on most USN liveries
+  {"wing_001", DIFFUSE, "empty", true};
+  {"wing_010", DIFFUSE, "empty", true};
+  {"wing_100", DIFFUSE, "empty", true};
+
+  -- 2-digit large nose numbers, used on USN/USMC aggressors
+  {"nose_aggressor_001", DIFFUSE, "a4e_bort_grey_small_1s", true};
+  {"nose_aggressor_010", DIFFUSE, "a4e_bort_grey_small_10s", true};
+
+  -- 3-digit large tail numbers, used on some USN/USMC aggressors
+  {"tail_001", DIFFUSE, "empty", true};
+  {"tail_010", DIFFUSE, "empty", true};
+  {"tail_100", DIFFUSE, "empty", true};
+}
