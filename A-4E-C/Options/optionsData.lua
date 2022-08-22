55fa2aee2148c009e2da417d7a6e306cd94bb615
Partario Flynn <partario.flynn@gmail.com>
2022-08-21 23:10:33 -0400

    special menu clean-up

--- A-4E-C/Options/optionsData.lua
+++ A-4E-C/Options/optionsData.lua
@@ -1,15 +1,17 @@
 cdata =
 {
 
-	A4E_AIRCRAFTNAME                  		= _('A-4E-C Options'),
+	A4E_AIRCRAFTNAME                  		= _('A-4E-C'),
+
+	A4E_PROJECTPAGE          				= _('https://08jne01.github.io/community-a4e-c/'),
 
 	A4E_TRIMSPEED_PITCH_LABEL          		= _('Pitch Trim Speed'),
 	A4E_TRIMSPEED_ROLL_LABEL           		= _('Roll Trim Speed'),
 	A4E_TRIMSPEED_RUDDER_LABEL         		= _('Rudder Trim Speed'),
-	A4E_TRIM_SPEED_SLIDER_HELP        		= _('Adjust trim speed settings to a percentage that suits the input rate of your device.'),
+	A4E_TRIM_SPEED_SLIDER_HELP        		= _('Trim speeds for pitch, roll, and rudder (Default: 100%)'),
 
 	A4E_THROTTLE_RATE_LABEL	        		= _('Throttle Rate'),
-	A4E_THROTTLE_RATE_HELP	        		= _('Adjust percentage rate of change for Throttle Position Increment and Decrement.'),
+	A4E_THROTTLE_RATE_HELP	        		= _('Percentage rate of change for Throttle Position Increment/Decrement (Default: 2.5%)'),
 
 --	A4E_CATLAUNCHMODE_LABEL           		= _('Catapult Launch Mode'),
 --	A4E_CATLAUNCHMODE_HELP            		= _('Launch the catapult when throttle at MIL POWER.'),
@@ -22,17 +24,17 @@ cdata =
 --	A4E_EXPERIMENTAL_EFM_CHECK_HELP   		= _('Should the A-4E-C use the experimental external flight model or the simple flight model.'),
 
 	A4E_COCKPIT_SHAKE_SLIDER_LABEL 	  		= _('Cockpit Shake'),
-	A4E_COCKPIT_SHAKE_SLIDER_HELP	  		= _('Adjust cockpit and head position shaking experienced.'),
+	A4E_COCKPIT_SHAKE_SLIDER_HELP	  		= _('Cockpit and head position shake (Default: 100%)'),
 
 	A4E_COCKPIT_CSS_LABEL			  		= _('FFB CSS Mode Deflection'),
-	A4E_COCKPIT_CSS_HELP			  		= _('Adjust % of stick deflection at which AFCS switches to Control Stick Steering (FFB sticks only).'),
+	A4E_COCKPIT_CSS_HELP			  		= _('Stick deflection percentage at which AFCS switches to Control Stick Steering (FFB sticks only)'),
 
 	A4E_WHEEL_BRAKE_ASSIST_CHECK_LABEL 		= _('Simple Braking (Rudder-Assisted)'),
 	A4E_WHEEL_BRAKE_ASSIST_CHECK_TOOLTIP 	= _('Simple Braking Enabled'),
-	A4E_WHEEL_BRAKE_ASSIST_CHECK_HELP  		= _('When enabled, the Wheel Brake binding applies to the Left and Right Wheelbrakes in proportion to current rudder input.'),
+	A4E_WHEEL_BRAKE_ASSIST_CHECK_HELP  		= _('When enabled, Wheel Brake applies Left and Right Wheelbrakes in proportion to rudder input'),
 
 	A4E_OLD_RADAR_CHECK_LABEL 				= _('AN/APG-53A Radar Legacy Mode'),
 	A4E_OLD_RADAR_CHECK_TOOLTIP 			= _('AN/APG-53A Radar Legacy Mode Enabled'),
-	A4E_OLD_RADAR_CHECK_HELP 				= _('Enables low-definition radar (performance mode).'),
+	A4E_OLD_RADAR_CHECK_HELP 				= _('Enables low-definition ground radar (performance mode)'),
 
 }
